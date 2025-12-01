13c8285d5bc8d8a294a33e85cd8892fd51e3ed54
blaise <blaise.tf01@gmail.com>
2025-12-01 09:26:21 +0000

    add and link user profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
+def profile(request):
+    """
+    View to display and manage user profile.
+    Renders the profile.html template with user information.
+    @login_required ensures only authenticated users can access this view.
+    """
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'accounts/profile.html', context)
